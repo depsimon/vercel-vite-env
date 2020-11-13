dcb49fe2bfbd37cf54d31a7fb00696d12be67488
Simon Depelchin <dep.simon@gmail.com>
2020-11-13 17:28:56 +0100

    Add env variable

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>{{ envTitle }}</div>
 </template>
 
 <script>
@@ -10,6 +11,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      envTitle: import.meta.env.VITE_APP_TITLE
+    }
   }
 }
 </script>
